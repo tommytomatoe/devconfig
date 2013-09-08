224af5adf6f395651f0c8a171528d14e496fa708
Tommy Nguyen <tommy@classic.cx>
2013-09-07 23:49:15 -0400

    vim config

---
+++ vim/vim/template/template.h
@@ -0,0 +1,40 @@
+/* 
+ * sample.h 
+ * Copyright (C) 2013 Tommy Nguyen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* 
+ * @author Tommy Nguyen
+ * @version 1.0 01.01.2013
+ */
+
+#ifndef _SAMPLE_H_
+#define _SAMPLE_H_
+
+#define __DEBUG__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+  extern char sample(int);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _SAMPLE_H_ */
